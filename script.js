35099675e110262136d3b19d1137d30f31ecf313
ManuelArdia <2008.ardia@gmail.com>
2026-03-13 23:41:00 +0100

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-function toggleMenu() {
-    const menu = document.getElementById("menu");
-    menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
-}
+// SCROLL SMOOTH
 
-/* Effetto fade-in */
-const cards = document.querySelectorAll('.card');
-cards.forEach(card => {
-    card.style.opacity = '0';
-    card.style.transition = '1s';
-    setTimeout(() => { card.style.opacity = '1'; }, 300);
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior:"smooth"
+        });
+
+    });
+
+});
+
+
+// ANIMAZIONE SEZIONI
+
+const sections = document.querySelectorAll(".poet-section");
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = "translateY(0px)";
+
+        }
+
+    });
+
+});
+
+sections.forEach(section => {
+
+    section.style.opacity = 0;
+    section.style.transform = "translateY(40px)";
+    section.style.transition = "all 1s";
+
+    observer.observe(section);
+
 });
 
 
